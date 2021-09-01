1ec89c4b541450e96788651df83a8db5784ad85c
hao <hdyzlh@gmail.com>
2021-09-01 22:55:38 +0800

    项目初始化

---
+++ src/main/java/cn/codehao/liveresourcemanagement/LiveResourceManagementApplication.java
@@ -0,0 +1,13 @@
+package cn.codehao.liveresourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveResourceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveResourceManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codehao/liveresourcemanagement/LiveResourceMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codehao.liveresourcemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveResourceMangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
